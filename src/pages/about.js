37430b751c3ba5717ff15247ea846369ec9eaa7e
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-02-07 00:53:51 -0500

    feat(pages): Link pre-built gatsby components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "gatsby";
 
 const AboutPage = () => (
   <main>
     <title>About Page</title>
     <h1>About Us</h1>
+    <Link to="/">Back to Home</Link>
     <p>
       Hi there! I'm a proud creator of this site, which I built with Gatsby.
     </p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "gatsby";
 
 const IndexPage = () => (
   <main>
     <title>Home Page</title>
     <h1>Welcome to my Gatsby site!</h1>
+    <Link to="/about">About</Link>
     <p>I'm this by following the Gatsby Tutorial.</p>
   </main>
 );
