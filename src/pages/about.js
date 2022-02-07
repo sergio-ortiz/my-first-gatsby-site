4d804c1870580b34f1da0bebbadc93794067498c
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-02-07 02:51:47 -0500

    feat(pages): layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { Link } from "gatsby";
+import Layout from "../components/layout";
 
 const AboutPage = () => (
-  <main>
-    <title>About Page</title>
-    <h1>About Us</h1>
-    <Link to="/">Back to Home</Link>
+  <Layout pageTitle="About Us">
     <p>
       Hi there! I'm a proud creator of this site, which I built with Gatsby.
     </p>
-  </main>
+  </Layout>
 );
 
 export default AboutPage;
