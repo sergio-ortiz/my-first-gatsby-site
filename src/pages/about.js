4f14f77e6270193f9607ab46f3cc9a1ad5c672ee
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-02-07 00:31:47 -0500

    feat(about): gatsby page component

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutPage = () => (
+  <main>
+    <title>About Page</title>
+    <h1>About Us</h1>
+    <p>
+      Hi there! I'm a proud creator of this site, which I built with Gatsby.
+    </p>
+  </main>
+);
+
+export default AboutPage;
