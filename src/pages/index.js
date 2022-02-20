63218b737a5d79cb64c66c5935abc7220f8adb7b
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-02-19 21:13:05 -0500

    Finished Gatsby Tutorial Part 7

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,7 @@ import { StaticImage } from "gatsby-plugin-image";
 const IndexPage = () => (
   <Layout pageTitle="Home Page">
     <p>I'm making this by following the Gatsby Tutorial.</p>
-    <StaticImage
-      alt="Stack of JAM toast"
-      src="https://images.unsplash.com/photo-1620921575116-fb8902865f81?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80https://images.unsplash.com/photo-1620921575116-fb8902865f81?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80"
-    />
+    <StaticImage alt="Stack of JAM toast" src="../images/jamstack.jpeg" />
   </Layout>
 );
 
